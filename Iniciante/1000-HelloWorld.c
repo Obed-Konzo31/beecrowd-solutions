61e6c155f87bbde33fd9860603d49e1361344e0b
Okonzo <162786963+Obed-Konzo31@users.noreply.github.com>
2026-03-23 17:10:32 +0100

    feat: Adiciona solução do problema 1000

---
+++ Iniciante/1000-HelloWorld.c
@@ -0,0 +1,12 @@
+// Problema: 1000 - Hello World!
+// Link: https://judge.beecrowd.com/pt/problems/view/1000
+// Descrição: imprimir a mensagem "Hello World!" e em seguida o final de linha. 
+
+
+#include <stdio.h>
+ 
+int main() 
+{
+    printf("Hello World!\n");    
+    return 0;
+}
